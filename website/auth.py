0979288b226e9757a97b54abd6390c833722f906
Megan <megantech@hotmail.com>
2022-10-17 13:42:13 +0000

    created database in directory

--- website/auth.py
+++ website/auth.py
@@ -28,7 +28,7 @@ def sign_up():
         elif len(firstName) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
+            flash('Passwords do not match.', category='error')
         elif len(password1) < 7:
             flash('Password must be a minimum of 7 characters.', category='error')
         else:
