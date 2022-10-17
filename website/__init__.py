154e005f251213fb856add5015780d5351924631
Megan <megantech@hotmail.com>
2022-10-17 14:27:38 +0000

    add log in python

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,27 @@
 """Import to not store password in plain text"""
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User().query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!' category='success')
+            else:
+                flash('Incorrect password, try again.' category='error')
+        else: 
+            flash('Email does not exist.' category='error')
+
     return render_template("login.html", boolean=True)
 
 
@@ -25,7 +38,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User().query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists' category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 4 categories.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
